808f19d590e572b9c1584d3afcfe6483487c2a75
asantos <alvarosang2003@gmail.com>
2026-07-02 22:25:56 +0200

    feat: add bno055 wrapper

---
+++ include/bno055_wrapper/bno055_wrapper.h
@@ -0,0 +1,9 @@
+#ifndef BNO055_WRAPPER_H
+#define BNO055_WRAPPER_H
+
+void bno055_init_a();
+void bno055_read();
+void bno055_convert();
+void bno055_stop();
+
+#endif // BNO055_WRAPPER_H
--- src/main.c
+++ src/main.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include "adc.h"
+#include "bno055_wrapper/bno055_wrapper.h"
 #include "gpio.h"
 #include "i2c.h"
 #include "usb_device.h"
 #include "usbd_cdc_if.h"
-
 #include "utils/utils.h"
+
 #include <stdint.h>
 #include <string.h>
 #include <sys/_intsup.h>
@@ -32,6 +33,7 @@ int main(void) {
     MX_ADC1_Init();
     MX_I2C1_Init();
     MX_USB_DEVICE_Init();
+    bno055_init_a();
 
     uint32_t valor_adc;
     char msg[32];
@@ -46,6 +48,10 @@ int main(void) {
         snprintf(msg, sizeof(msg), "Hola mundo: %lu\r\n", valor_adc);
         CDC_Transmit_FS((uint8_t *)msg, (uint16_t)strlen(msg));
 
+        bno055_read();
+
         sleep(1);
     }
+
+    bno055_stop();
 }
